e2111d7e3c6894068401ab1a1566c33ec34c655c
Максим Оглобля <maxogloblia@gmail.com>
2025-02-07 14:21:23 +0200

    1

---
+++ src/test/java/HorseTest.java
@@ -0,0 +1,24 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class HorseTest {
+
+    @org.junit.jupiter.api.Test
+    void getName() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getSpeed() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getDistance() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void move() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getRandomDouble() {
+    }
+}
